07e9f9994beacf6d71a17fcd6faafc93573e2892
teich0psia <teich0psia@proton.me>
2026-08-31 08:14:06 +0900

    feat: add detached session cleanup watchdog entrypoint

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    if rivals_toolkit_lib::run_session_watchdog_from_args() {
+        return;
+    }
     rivals_toolkit_lib::run()
 }
